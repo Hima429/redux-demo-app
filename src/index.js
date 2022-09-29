11339c0b65651ec6895d57996b2adc1afdd893c0
Pippalla Hima <saisrivalli.pippalla@gmail.com>
2022-09-29 17:49:40 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -3,11 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { store } from "./store";
+import { Provider } from "react-redux";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import UserDetailsPage from "../src/userDetailsPage/index"
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/user" element={<UserDetailsPage />} />
+          </Routes>
+        </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
